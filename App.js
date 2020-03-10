aadb74540b06d57dd3148b09e7dc5c2f237969ad
Tran Tuan Anh <yorkittran@gmail.com>
2020-03-10 09:35:33 +0700

    Example BottomNavigation

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
-import { mapping, light as lightTheme } from '@eva-design/eva';
+import { mapping, light as theme } from '@eva-design/eva';
+import { AppNavigator } from './components/navigation.component';
 
 const HomeScreen = () => (
   <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
     <Text category='h1'>HOME</Text>
   </Layout>
 );
-  
+
 const App = () => (
-  <ApplicationProvider mapping={mapping} theme={lightTheme}>
-    <HomeScreen />
-  </ApplicationProvider>
+    <ApplicationProvider mapping={mapping} theme={theme}>
+      <HomeScreen/>
+      <AppNavigator/>
+    </ApplicationProvider>
 );
 
 export default App;
